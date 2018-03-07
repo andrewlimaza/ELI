a16d9ab11072de1376657234cdcf46193d909982
Andrew Lima <andrew@arctek.co.za>
2018-03-07 21:31:32 +0200

    Update to search page.
    Update search page to work.

--- search.php
+++ search.php
@@ -5,20 +5,29 @@
  * @package eli
  */
 get_header(); ?>
-	<section id="primary" class="columns content-area">
-		<?php do_action('before_main'); ?>
-		<main id="main" class="site-main" role="main">
-		<?php do_action('before_loop'); ?>
-		<?php if ( have_posts() ) : ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'search' );	?>
-			<?php endwhile; ?>
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
-		<?php do_action('after_loop'); ?>
-		</main><!-- #main -->
-		<?php do_action('after_main'); ?>
-	</section><!-- #primary -->
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<div class="container-fluid" style="padding:2rem;">
+	<div class="container">
+		<h1><?php _e( 'Search Page', 'eli' ); ?></h1>
+		<?php 
+		get_search_form();
+		
+		global $wp_query;
+		$total_results = $wp_query->found_posts;
+
+		if( ! empty( $total_results ) ){
+			echo '<h2 id="eli_s_posts_found">' . $total_results . " " . __( "Posts Found.", "eli" ) . '</h2>';
+
+			foreach( $wp_query->posts as $post ){				
+				echo '<li><a href="' .$post->guid . '">' . $post->post_title . '</a></li>';
+			}
+
+		}else{
+			echo '<h2 id="eli-no-posts-found" style="text-align:center;">';
+			_e( 'No posts found, please search with a different keyword.', 'eli' );
+			echo '</h2>';
+		}
+	?>
+	</div>
+</div>
+
+<?php get_footer();
