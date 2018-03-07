62058a7ed1969b7d5ec7d4b5ead1e880abe59e6e
Andrew Lima <andrew@arctek.co.za>
2018-03-07 21:33:07 +0200

    Adjustments to 404 page.
    Styling improvements to 404 page.

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@ get_header(); ?>
 
 <div class="container eli-content-container">
 	<div class="row">
-		<div class="col-lg-8 col-md-12 col-sm-12">
+		<div class="col-lg-12 col-md-12 col-sm-12 text-center">
 			<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'eli' ); ?></h1>
 			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'eli' ); ?></p>
 			<?php get_search_form(); ?>				
