dba8b1db1d34e992292451ec373817598d7f94b7
Andrew Lima <andrew@arctek.co.za>
2018-04-23 18:41:34 +0200

    Updated styling + comments.

--- comments.php
+++ comments.php
@@ -2,15 +2,6 @@
 /**
  * The template for displaying comments
  *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
  */
 
 /*
@@ -33,7 +24,7 @@ if ( post_password_required() ) {
 			$comments_number = get_comments_number();
 			if ( '1' === $comments_number ) {
 				/* translators: %s: post title */
-				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'twentyseventeen' ), get_the_title() );
+				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'eli' ), get_the_title() );
 			} else {
 				printf(
 					/* translators: 1: number of comments, 2: post title */
@@ -42,7 +33,7 @@ if ( post_password_required() ) {
 						'%1$s Replies to &ldquo;%2$s&rdquo;',
 						$comments_number,
 						'comments title',
-						'twentyseventeen'
+						'eli'
 					),
 					number_format_i18n( $comments_number ),
 					get_the_title()
@@ -57,7 +48,7 @@ if ( post_password_required() ) {
 					'avatar_size' => 100,
 					'style'       => 'ol',
 					'short_ping'  => true,
-					'reply_text'  => __( 'Reply', 'twentyseventeen' ),
+					'reply_text'  => __( 'Reply', 'eli' ),
 				) );
 			?>
 		</ol>
@@ -73,7 +64,7 @@ if ( post_password_required() ) {
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentyseventeen' ); ?></p>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'eli' ); ?></p>
 	<?php
 	endif;
 
