d99f38a6d7fc86eed64492712412c98b8161727e
Andrew Lima <andrew@arctek.co.za>
2018-05-15 12:18:37 +0200

    Single Post H1 Tag
    If it's single post it should have H1 tags, not h2.

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -20,7 +20,7 @@
 			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 		'</a></h2>' ); 
 		} else {
-			the_title( '<h2 class="entry-title">' );
+			the_title( '<h1 class="entry-title">', '</h1>' );
 		}
 		?>
 
